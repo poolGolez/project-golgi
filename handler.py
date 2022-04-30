1e079a785ed028adcfc49adec0b1aab0c1b988ce
pool <loop.edward@gmail.com>
2022-04-30 14:02:42 +0800

    Retrieve S3 location from S3 event

--- handler.py
+++ handler.py
@@ -1,10 +1,15 @@
 import json
-
+import urllib.parse
 
 def generate_thumbnail(event, context):
+
+    first_record = event["Records"][0]
+
+    bucket_name = first_record["s3"]["bucket"]["name"]
+    object_key = urllib.parse.unquote_plus(first_record["s3"]["object"]["key"], encoding='utf-8')
     body = {
-        "message": "Go Serverless v3.0! Your function executed successfully!",
-        "input": event,
+        "message": "Success, buddy! You've got the S3 location",
+        "s3": "{bucket_name}/{object_key}",
     }
 
     return {"statusCode": 200, "body": json.dumps(body)}
