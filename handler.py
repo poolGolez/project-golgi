23bdb67ecead7b99645f140673fc9ebcfafb912e
pool <loop.edward@gmail.com>
2022-04-30 14:51:14 +0800

    Retrieve S3 object content

--- handler.py
+++ handler.py
@@ -1,12 +1,19 @@
 import json
+from re import S
+import boto3
 import urllib.parse
 
-def generate_thumbnail(event, context):
+s3 = boto3.client("s3")
 
+def generate_thumbnail(event, context):
     first_record = event["Records"][0]
 
     bucket_name = first_record["s3"]["bucket"]["name"]
     object_key = urllib.parse.unquote_plus(first_record["s3"]["object"]["key"], encoding='utf-8')
+
+    response = s3.get_object(Bucket=bucket_name, Key=object_key)
+    s3_object = response["Body"].read()
+
     body = {
         "message": "Success, buddy! You've got the S3 location",
         "s3": "{bucket_name}/{object_key}",
