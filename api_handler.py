bcfdbe8f1ea3b25238bf5b7823de68a157c5efbf
pool <loop.edward@gmail.com>
2022-05-04 20:56:44 +0800

    Add fetching of all records from dynamoDB

--- api_handler.py
+++ api_handler.py
@@ -1,7 +1,25 @@
+import boto3
 import json
+import os
+
+dynamodb = boto3.client("dynamodb")
+metadata_table = os.environ["METADATA_TABLE"]
+
 
 def list(event, context):
+    response = dynamodb.scan(TableName=metadata_table)
+
+    body = parse_items(response["Items"])
+
     return {
         "statusCode": 200,
-        "body": json.dumps(event)
+        "body": json.dumps(body)
     }
+
+
+def parse_items(items):
+    return [{
+        "file_location": item["file_location"]["S"],
+        "thumbnail_location": item["thumbnail_location"]["S"],
+        "file_size": int(item["file_size"]["N"])
+    } for item in items]
